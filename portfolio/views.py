62f50508a97f4b456fb329c41abe503df35862d2
Christian Moreno <christianmoreno@Christians-MacBook-Pro.local>
2021-01-20 06:20:16 -0700

    Fixed deleted and started a new database since there variable inconsistencies.  Also added user profile update and user images next to Project

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
-from.models import Post
+from .models import Project
 
 projects = [
 	{
 	'name': 'Subreddit Keyword Search',
 	'language': "Python",
 	'description': 'Created an application that searches r/ProgrammerHumor using any keyword and populates an excel file in order of the score',
-	'date_completed': 'October 14, 2020'
+	'date_completed': 'October 14, 2020',
 	},
 	{
 	'name': 'Website portfolio',
 	'language': "Python, Django",
 	'description': 'Created this website using Django and programmed the server using Linode',
-	'date_completed': 'January 11, 2020'
+	'date_completed': 'January 11, 2020',
 	},
 	
 
@@ -20,7 +20,7 @@ projects = [
 
 def home(request):
 	context = {
-		'projects': projects
+		'projects': Project.objects.all()
 	}
 	return render(request, 'portfolio/home.html', context)
 
