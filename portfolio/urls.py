f8949daac4681c36101b201448709c5f117def22
Christian Moreno <christianmoreno@Christians-MacBook-Pro.local>
2021-01-21 17:15:38 -0700

    readded default.jpg and fixed Profile models issue for 'force_insert' error

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,7 +7,7 @@ from .views import (
 	ProjectDeleteView
 )
 from . import views
-# from users import views as user_views
+from users import views as user_views
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='portfolio_home'),
@@ -15,6 +15,6 @@ urlpatterns = [
     path('project/new/', ProjectCreateView.as_view(), name='project_create'),
     path('project/<int:pk>/update/', ProjectUpdateView.as_view(), name='project_update'),
     path('project/<int:pk>/delete/', ProjectDeleteView.as_view(), name='project_delete'),
-    # path('register/', user_views.register, name='register'),
+    path('register/', user_views.register, name='register'),
     path('about/', views.about, name='portfolio_about'),
 ]
